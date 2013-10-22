0806a973a6e3e975282adcaba40063bed479f5b2
Sung Hoon (Kevin) Kim <sungkim7790@gmail.com>
2013-10-21 17:43:38 -0700

    updated motorcontroller class, including new methods

--- sistineChapel/motorController.cpp
+++ sistineChapel/motorController.cpp
@@ -1,39 +1,11 @@
 #import <Arduino.h>
 #include "motorController.h"
 
-MotorController :: motorController()
+MotorController::motorController()
 {
-	myMotorNum = 0;
-	myDutyCycle = 0.0;
 }
 
-MotorController :: motorController(int motorNum)
-{
-	myMotorNum = motorNum;
-	myDutyCycle = 0.0;
-}
-
-void MotorController :: setMotor(int motorNum)
-{
-	myMotorNum = motorNum;
-}
-
-int MotorController :: getMotor()
-{
-	return myMotorNum;
-}
-
-void MotorController :: setDutyCycle(float dutyCycle)
-{
-	myDutyCycle = dutyCycle;
-}
-
-float MotorController :: getDutyCycle()
-{
-	return myDutyCycle;
-}
-
-void setPwm(int motorNum, float dutyCycle)
+void MotorController::setPwm(int motorNum, float dutyCycle)
 {
 	if(dutyCycle > 100)
 	{
@@ -43,12 +15,13 @@ void setPwm(int motorNum, float dutyCycle)
 	{
 		dutyCycle = 0;
 	}
+
 	int pin = 3 * (motorNum-1); 		//selects the first pin on the pwm driver
 	float rate = dutyCycle * 40.95; 	//calculates the integer value for the desired duty cycle
 	pwm.setPWM(pin + 2, 0, int(rate));
 }
 
-void forward(int motorNum)
+void MotorController::forward(int motorNum)
 {
 	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
     
@@ -56,7 +29,7 @@ void forward(int motorNum)
 	pwm.setPWM(pin + 1, 0, 4095);	//Sets the INB pin of the motor to High
 }
 
-void reverse(int motorNum)
+void MotorController::reverse(int motorNum)
 {
 	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
   
@@ -64,10 +37,37 @@ void reverse(int motorNum)
 	pwm.setPWM(pin + 1, 4095, 0);	//Sets the INB pin of the motor to LOW
 }
 
-void brake(int motorNum)
+void MotorController::speed(int motorNumber, int speed)
+{
+	if(speed > 0)
+	{
+		forward(motorNumber);
+		setPwm(motorNumber, speed);
+	}
+	else if(speed < 0)
+	{
+		reverse(motorNumber);
+		speed *= -1;
+		setPwm(motorNumber, speed);
+	}
+	else
+	{
+		setPwm(motorNumber, 0);
+	}
+}
+
+void MotorController::coast(int motorNumber)
+{
+	int pin = 3 * (motorNumber - 1); //selects the first pin on the pwm driver
+  
+	pwm.setPWM(pin, 4095, 0); //Sets the INA pin of the motor to Low
+	pwm.setPWM(pin + 1, 4095, 0); //Sets the INB pin of the motor to Low
+}
+
+void MotorController::brake(int motorNum)
 {
 	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
   
 	pwm.setPWM(pin, 0, 4095);	//Sets the INA pin of the motor to High
 	pwm.setPWM(pin + 1, 0, 4095);	//Sets the INB pin of the motor to High
-}
+} 
--- sistineChapel/motorController.h
+++ sistineChapel/motorController.h
@@ -1,36 +1,27 @@
-#pragma once
+#ifndef MOTORCONTROLLER_H
+#define MOTORCONTROLLER_H
 
 class MotorController
 {
-	private:
-		//current motor number 1 - 5
-		int myMotorNum;
-		//current dutycycle 0 - 100
-		float myDutyCycle;
-		
-	public:
-		//contructor
-		//creates motorController object; does not have specified motor number, which is set to 0
-		//duty cycle is set to 0
-		motorController();
-		//contructor
-		//creates motorController object with specified motor number
-		//duty cycle is set to 0
-		motorController(int motorNum);
-		//sets motorController specified motor number
-		void setMotor(int motorNum);
-		//returns motor number
-		int getMotor();
-		//sets motorControoler specified dutycycle
-		void setDutyCycle(float dutyCycle);
-		//returns duty cycle
-		float getDutyCycle();
+	private:	
 		//sets the PWM duty cycle for the desired motor
 		void setPwm(int motorNum, float dutyCycle);
 		// On Call Motor is set to move Forward.
 		void forward(int motorNum);
 		// On Call Motor is set to move Reverse
 		void reverse(int motorNum);
+
+	public:
+		//empty contructor that creates motorController.
+		motorController();
+		//Sets specified motor to specified dutycycle, speed.
+		//motorNumber = 1 to 5
+		//speed = -100 to 100
+		void speed(int motorNumber, int speed);
+		//On call motor is reset, all pins set to Low. Motor is set to coast.
+		void coast(int motorNumber);
 		// On call motor is reset, all pins set to HIGH. Motor is Stopped.
-		void brake(int motorNum);
+		void brake(int motorNumber);
 };
+
+#endif
