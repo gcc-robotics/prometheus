1f32da03e8cb2e56619bb15f9b4966ea3ddbc579
Sung Hoon (Kevin) Kim <sungkim7790@gmail.com>
2013-10-21 17:10:02 -0700

    added motorcontroller class

--- sistineChapel/motorController.cpp
+++ sistineChapel/motorController.cpp
@@ -1,12 +1,73 @@
+#import <Arduino.h>
 #include "motorController.h"
 
-void CRectangle::set_values(int a, int b)
+MotorController :: motorController()
 {
-  x = a;
-  y = b;
+	myMotorNum = 0;
+	myDutyCycle = 0.0;
 }
 
-int CRectangle::area()
+MotorController :: motorController(int motorNum)
 {
-	return x * y;
+	myMotorNum = motorNum;
+	myDutyCycle = 0.0;
+}
+
+void MotorController :: setMotor(int motorNum)
+{
+	myMotorNum = motorNum;
+}
+
+int MotorController :: getMotor()
+{
+	return myMotorNum;
+}
+
+void MotorController :: setDutyCycle(float dutyCycle)
+{
+	myDutyCycle = dutyCycle;
+}
+
+float MotorController :: getDutyCycle()
+{
+	return myDutyCycle;
+}
+
+void setPwm(int motorNum, float dutyCycle)
+{
+	if(dutyCycle > 100)
+	{
+		dutyCycle = 100;
+	}
+	if(dutyCycle < 0)
+	{
+		dutyCycle = 0;
+	}
+	int pin = 3 * (motorNum-1); 		//selects the first pin on the pwm driver
+	float rate = dutyCycle * 40.95; 	//calculates the integer value for the desired duty cycle
+	pwm.setPWM(pin + 2, 0, int(rate));
+}
+
+void forward(int motorNum)
+{
+	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
+    
+	pwm.setPWM(pin, 4095, 0);	//Sets the INA pin of the motor to LOW
+	pwm.setPWM(pin + 1, 0, 4095);	//Sets the INB pin of the motor to High
+}
+
+void reverse(int motorNum)
+{
+	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
+  
+	pwm.setPWM(pin, 0, 4095);	//Sets the INA pin of the motor to HIGH
+	pwm.setPWM(pin + 1, 4095, 0);	//Sets the INB pin of the motor to LOW
+}
+
+void brake(int motorNum)
+{
+	int pin = 3 * (motorNum - 1);	//selects the first pin on the pwm driver
+  
+	pwm.setPWM(pin, 0, 4095);	//Sets the INA pin of the motor to High
+	pwm.setPWM(pin + 1, 0, 4095);	//Sets the INB pin of the motor to High
 }
--- sistineChapel/motorController.h
+++ sistineChapel/motorController.h
@@ -1,8 +1,36 @@
-class CRectangle 
+#pragma once
+
+class MotorController
 {
 	private:
-		int x, y;
+		//current motor number 1 - 5
+		int myMotorNum;
+		//current dutycycle 0 - 100
+		float myDutyCycle;
+		
 	public:
-		void set_values(int,int);
-		int area(void);
+		//contructor
+		//creates motorController object; does not have specified motor number, which is set to 0
+		//duty cycle is set to 0
+		motorController();
+		//contructor
+		//creates motorController object with specified motor number
+		//duty cycle is set to 0
+		motorController(int motorNum);
+		//sets motorController specified motor number
+		void setMotor(int motorNum);
+		//returns motor number
+		int getMotor();
+		//sets motorControoler specified dutycycle
+		void setDutyCycle(float dutyCycle);
+		//returns duty cycle
+		float getDutyCycle();
+		//sets the PWM duty cycle for the desired motor
+		void setPwm(int motorNum, float dutyCycle);
+		// On Call Motor is set to move Forward.
+		void forward(int motorNum);
+		// On Call Motor is set to move Reverse
+		void reverse(int motorNum);
+		// On call motor is reset, all pins set to HIGH. Motor is Stopped.
+		void brake(int motorNum);
 };
