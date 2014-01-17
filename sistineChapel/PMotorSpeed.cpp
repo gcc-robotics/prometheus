d788879359a5a028bf41409dfd0023db2fe4fb2e
sungkim7790 <sungkim7790@gmail.com>
2014-01-17 13:14:37 -0800

    Saved copy of the PMotorSpeed class

--- sistineChapel/PMotorSpeed.cpp
+++ sistineChapel/PMotorSpeed.cpp
@@ -11,11 +11,11 @@ PMotorSpeed::PMotorSpeed()
 		this->setPoint(i, 0);
 	}
 
-	proportionalgain[0] = 0.0;
-	proportionalgain[1] = 0.0;
-	proportionalgain[2] = 0.0;
-	proportionalgain[3] = 0.0;
-	proportionalgain[4] = 0.0;
+	proportionalgain[0] = 1.0;
+	proportionalgain[1] = 1.0;
+	proportionalgain[2] = 1.0;
+	proportionalgain[3] = 1.0;
+	proportionalgain[4] = 1.0;
 
 	min[0] = 0.0;
 	min[1] = 0.0;
@@ -30,21 +30,32 @@ PMotorSpeed::PMotorSpeed()
 	max[4] = 359.9;
 }
 
-int PMotorSpeed::calculate(float currentAngleError)
+void PMotorSpeed::setProportionalGain(int motorNumber, float proportionalgain)
 {
+	motorNumber = clamp(motorNumber, 0, 5);
+	proportionalgain = clamp(proportionalgain, 0.0, 5.0);
 
+	proportionalgain[motorNumber] = proportionalgain;
+}
+
+int PMotorSpeed::calculate(int motorNumber, float currentAngleError)
+{
+	motorNumber = clamp(motorNumber, 0, 5);
+	currentAngleError = clamp(currentAngleError, -360.0, 360.0);
+
+	return currentAngleError * proportionalgain[motorNumber];
 }
 
 void PMotorSpeed::setPoint(int motorNumber, float setPoint)
 {
 	motorNumber = clamp(motorNumber, 0, 5);
 	
-	if(setPoint < 0)
+	if(setPoint < 0.0)
 	{
-		setPoint = setPoint * -1;
+		setPoint = setPoint * -1.0;
 	}
 
-	setPoint = fmod(setPoint, 360);
+	setPoint = fmod(setPoint, 360.0);
 	
 	if(setPoint < min[motorNumber])
 	{
